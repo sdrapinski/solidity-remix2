221493456357d82820fdd4937608d7a1985af95d
sdrapinski <szymondrapinski@gmail.com>
2023-12-07 00:46:09 +0100

    fund me finish

--- FundMe.sol
+++ FundMe.sol
@@ -1,18 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public minimumUsd = 5e18;
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded)
         public addressToAmountfunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(
-            getConversionRate(msg.value) >= minimumUsd,
+            msg.value.getConversionRate() >= minimumUsd,
             "didnt send enough ETH"
         ); //1e18 = 1ETH
         funders.push(msg.sender);
@@ -21,29 +29,34 @@ contract FundMe {
             msg.value;
     }
 
-    function getPrice() public view returns (uint256) {
-        //address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        //abi
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountfunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // you can withdraw money by transfer, call or send
 
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        (, int256 price, , , ) = priceFeed.latestRoundData();
+        //transfer
+        // payable(msg.sender).transfer(address(this).balance);
 
-        return uint256(price * 1e10);
+        // //send
+        // bool sendSuccess =  payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
     }
 
-    function getConversionRate(
-        uint256 ethAmount
-    ) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
-
-    function getVerions() public view returns (uint256) {
-        return
-            AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306)
-                .version();
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Must be owner");
+        _;
     }
 }
